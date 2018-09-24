448b7bd74950ab143c05ca6d88bb05ea4938922b
farverio <github email address>
2018-09-24 09:19:09 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  state = {
+    peopleInSpace: []
+  }
+
   componentDidMount() {
     fetch('http://api.open-notify.org/astros.json')
-      .then(res => res.json())  
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })  
   }
   
   render() {
     return (
-      <div>
-        
-      </div>
+      <ul>
+        {this.state.peopleInSpace.map(person => <li>{person.name}</li>)}
+      </ul>
     )
   }
 }
